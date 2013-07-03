d816c4e98a3136e84a476787b1eab043216d4854
victoria <victoria@victoria-pc>
2013-07-03 09:28:33 -0500

    made form for page

---
+++ major-minor.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <link rel="stylesheet" href="global.css" type="text/css">
+    </head>
+    <body>
+        <?php include "navigation.php"; ?>
+        <div class="wrapper">
+            <form> Minor Offense: <input type="text" name="minor offense" /> <br />
+                <input type="radio" name="level" value="Level 1" />Level 1<br />
+                <input type="radio" name="level" value="Level 2" />Level 2<br />
+                <input type="radio" name="level" value="Level 3" />Level 3<br />
+                <input type="radio" name="level" value="Level 4" />Level 4<br />
+                <input type="radio" name="level" value="Level 5" />Level 5<br />
+                <input type="Submit" value="Submit" /><br />
+                Major Offense: <input type="text" name="major offense" /><br />
+                <input type="radio" name="level" value="Level 1" />Level 1<br />
+                <input type="radio" name="level" value="Level 2" />Level 2<br />
+                <input type="radio" name="level" value="Level 3" />Level 3<br />
+                <input type="radio" name="level" value="Level 4" />Level 4<br />
+                <input type="radio" name="level" value="Level 5" />Level 5<br />
+                <input type="Submit" value="Submit" /><br />
+                
+            </form>
+        </div>
+    </body>
+</html>
