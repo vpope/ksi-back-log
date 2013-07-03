40f0eed25b2516201af8022f24397b2a3097c2ed
victoria <victoria@victoria-pc>
2013-07-03 09:28:48 -0500

    made form

---
+++ release.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <link rel="stylesheet" href="global.css" type="text/css">
+    </head>
+    <body>
+        <?php include "navigation.php"; ?>
+        <div class="wrapper">
+            Release Date: <input type="text" name="release date" /><br />
+            <input type="Submit" value="Submit" /><br />
+        </div>
+    </body>
+
+</html>
