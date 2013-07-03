41456f3b50ae30eb250c53bddd45db128d12ec6b
victoria <victoria@victoria-pc>
2013-07-03 09:28:12 -0500

    set up homepage

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <link rel="stylesheet" href="global.css" type="text/css">
+    </head>
+    <body>
+        <?php include "navigation.php"; ?>
+        <div class="wrapper">
+            hello hello hello test!
+            
+        </div>
+    </body>
+    
+</html>
